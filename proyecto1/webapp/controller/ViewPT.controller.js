fb68b6bdfbef0fd506ffab358d791fa2599dfaa0
KarinaAgz <karinaangon10@gmail.com>
2025-01-28 18:24:09 +0000

    cambios en vista facturaciones 28/01/2025

--- proyecto1/webapp/controller/ViewPT.controller.js
+++ proyecto1/webapp/controller/ViewPT.controller.js
@@ -33,36 +33,24 @@ sap.ui.define([
             var oRouter = this.getOwnerComponent().getRouter();
             oRouter.navTo("main");
         },
-        onFilterTable: function () {
-            var oTable = this.getView().byId("idTablePT");
+        onFilterTable: function (oEvent) {
+            var sFecha = this.byId("idFecha").getValue();
+            var oTable = this.byId("idTablePT");
             var oBinding = oTable.getBinding("items");
         
-            // Obtén el valor del DatePicker
-            var sFecha = this.getView().byId("idFecha").getValue();
-            var aFilters = [];
-        
-            if (sFecha) {
-                // Separar la fecha en partes (día, mes, año)
-                var aFechaParts = sFecha.split('/'); // Asume el formato DD/MM/YYYY
-                if (aFechaParts.length === 3) {
-                    var sDay = aFechaParts[0];
-                    var sMonth = aFechaParts[1];
-                    var sYear = aFechaParts[2];
-        
-                    // Convertir al formato YYYY-MM-DD
-                    var sFormattedDate = `${sYear}-${sMonth.padStart(2, '0')}-${sDay.padStart(2, '0')}`;
-                    console.log("Fecha convertida al formato ISO:", sFormattedDate);
-        
-                    // Crear el filtro con la fecha formateada
-                    aFilters.push(new sap.ui.model.Filter("FDesde", sap.ui.model.FilterOperator.EQ, sFormattedDate));
-                } else {
-                    console.error("Formato de fecha inválido:", sFecha);
-                }
+            // Si no hay fecha, limpiar los filtros directamente
+            if (!sFecha) {
+                oBinding.filter([]); // Limpia todos los filtros
+                return; // Sal del método
             }
         
-            // Aplicar el filtro a la tabla
-            oBinding.filter(aFilters);
-        }
+            // Si hay fecha, aplica el filtro
+            var oFilter = new sap.ui.model.Filter("FDesde", sap.ui.model.FilterOperator.EQ, sFecha);
+            oBinding.filter([oFilter]);
+        },
+        
+        
+        
         
         
     });
